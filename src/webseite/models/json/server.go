21f6a2b12bac4e9d2612a31121b43efb02f225c7
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2015-01-02 01:15:16 +0100

    Revert the order to read

--- src/webseite/models/json/server.go
+++ src/webseite/models/json/server.go
@@ -95,7 +95,7 @@ func ReloadServers(servers []models.Server) {
 		var jsonPing Ping
 
 		for pingI := range sqlServer.Pings {
-			sqlPing := sqlServer.Pings[pingI]
+			sqlPing := sqlServer.Pings[len(sqlServer.Pings) - pingI]
 
 			jsonPing = Ping{
 				Online: sqlPing.Online,
