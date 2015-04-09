4d41e828fbdee99864100a7841cbf87b306601bb
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2015-04-09 21:22:32 +0200

    Only send maxplayers for visible Servers

--- src/webseite/models/json/maxPlayer.go
+++ src/webseite/models/json/maxPlayer.go
@@ -24,5 +24,21 @@ func (j *JSONMaxPlayerResponse) Broadcast() {
 		Value: j,
 	}
 
-	jsonResponse.Broadcast()
+	jsonBytes := jsonResponse.marshal()
+	if len(jsonBytes) > 0 {
+		for c := range websocket.Hub.Connections {
+			allowedServers := c.Session.Get("servers").(map[int32]bool)
+			if val, ok := allowedServers[j.Id]; !ok || !val {
+				continue
+			}
+
+			select {
+			case c.Send <- jsonBytes:
+			default:
+				c.CloseCustomChannels()
+				close(c.Send)
+				delete(websocket.Hub.Connections, c)
+			}
+		}
+	}
 }
